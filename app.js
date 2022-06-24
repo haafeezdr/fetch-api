4d813aeb5b092259e3a84fd49c5915d9b43df1ad
haafeez <haafeezdaiyab@gmail.com>
2022-06-23 18:36:46 -1200

    initial commit

--- app.js
+++ app.js
@@ -39,7 +39,7 @@
 
   // get from external Api  
   function getJson() {
-    fetch('post.json')
+    fetch('https://api.github.com/users')
     .then(function(res){
         return res.json();
     })
